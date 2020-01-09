074a26affa3eec6ec322a7d65b002fc11103eaa3
Jesse Golding <jessegolding05@gmail.com>
2020-01-09 17:28:24 +0000

    latest

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -1,39 +1,58 @@
 function init() {
   //  dom variables
   const grid = document.querySelector('.grid')
-  const squares = []
+  const btn = document.querySelector('.start')
+  const scoreDisplay = document.querySelector('.score')
+  let squares = []
   let dir = null
   let pace = 500
-  let timerId = setInterval(snakeMovement, pace)
+  // eslint-disable-next-line prefer-const
+  let timerId = null
+  //let gameInPlay = null
+
 
   //const scoreDisplay = document.querySelector('.score') //will display the players score during in game play
   // game variables
   const width = 11
-  const snake = [3, 2, 1]
-  //let score = 0 //score variable so the player can see their score 
+  let snake = [62, 61, 60]
+  let score = 0 //score variable so the player can see their score 
   // loop as many times as width times the width to fill the grid
-  Array(width * width).join('.').split('.').forEach((i, ind) => {
-    // create 
-    const square = document.createElement('div')
-    square.classList.add('grid-item')
-    square.innerHTML = ind + '-' + ind % width
-    squares.push(square)
-    grid.appendChild(square)
-  })
+  // Array(width * width).join('.').split('.').forEach((i, ind) => {
+  //   // create 
+  //   const square = document.createElement('div')
+  //   square.classList.add('grid-item')
+  //   square.innerHTML = ind + '-' + ind % width
+  //   squares.push(square)
+  //   grid.appendChild(square)
+  // })
+
+  function newGrid() {
+    Array(width * width).join('.').split('.').forEach((i, ind) => {
+      // create 
+      const square = document.createElement('div')
+      square.classList.add('grid-item')
+      square.innerHTML = ind + '-' + ind % width
+      squares.push(square)
+      grid.appendChild(square)
+    })
+  }
 
   function addSnake() {
+    console.log('snake added')
     snake.map(index => squares[index].classList.add('player'))
   }
-  addSnake()
+
+  // addSnake()
 
   function removeSnake() {
     snake.map(index => squares[index].classList.remove('player'))
   }
+
   function apple() {
     const appleIndex = Math.floor(Math.random() * squares.length)
     squares[appleIndex].classList.add('apple')
   }
-  apple()
+  // apple()
   console.log(squares[snake[0]])
 
 
@@ -44,24 +63,32 @@ function init() {
       snake.unshift(snake[0])
       //console.log('apple')
       apple()
+      score += 100
+      pace -= 100
       snakeMovement()
-      increaseSpeed()
-
+      scoreDisplay.innerHTML = score
     }
 
   }
-  function increaseSpeed() {
-    
-    pace = pace - 100
-    timerId = setTimeout(snakeMovement, pace)
 
+  function startGame() {
+    // grid.innerHTML = ''
+    newGrid()
+    addSnake()
+    snakeMovement()
+    
+    apple()
+    //we add the new interval for the new game - with the right page
+    timerId = setInterval(snakeMovement, pace)
   }
+  
   console.log(dir)
   // places player at the starting position when grid has finished building and the game is starting again
   //squares[snake].classList.add('player')
 
   function snakeMovement() {
     //console.log(dir)
+    console.log('snakeMove')
     if (dir === 'right') {
       console.log(dir)
       removeSnake()
@@ -107,8 +134,10 @@ function init() {
     }
     snakeEatsApple()
     hit()
+
     //snakeMovement()
   }
+  startGame()
   console.log(squares)
   // snakeMovement()
 
@@ -119,11 +148,34 @@ function init() {
     }
 
   }
+
   function gameOver() {
-    console.log('game over')
+    
     clearInterval(timerId)
     grid.innerHTML = ''
     grid.innerHTML = '<p>game over</p>'
+    //reset()
+    
+  }
+
+  function reset() {
+    //clear the grid (including the squares array)
+    clearInterval(timerId)
+    grid.innerHTML = ''
+    squares = []
+    score = 0
+    scoreDisplay.innerHTML = score
+    pace = 500
+    snake = [62, 61, 60]
+    dir = null
+    // removeSnake()
+    //clear the interval from the previous game
+    
+    //create a new grid and add the food
+    
+    //inside of start game there is addSnake() and snakeMovement() functions
+    startGame()
+
   }
 
   function handleKeyDown(e) {
@@ -131,73 +183,30 @@ function init() {
     switch (e.keyCode) {
       case 39: if (dir !== 'left') dir = 'right'
 
-       
+
         break
-      case 37: if (dir !== 'right') dir = 'left'
-       
+      case 37: if (dir !== 'right' && dir !== null) dir = 'left' //moveLeft()
+
 
         break
       case 40: if (dir !== 'up') dir = 'down'
-        
+
 
         break
       case 38: if (dir !== 'down') dir = 'up'
-        
+
 
         break
       default:
     }
   }
 
-  //squares.forEach(square => square.classList.remove('player'))
-  //squares[snake].classList.add('player')
-  console.log('current plauer index is', snake)
+  console.log('current player index is', snake)
 
+  // event handlers
 
   window.addEventListener('keydown', handleKeyDown)
+  btn.addEventListener('click', reset)
 }
 
-// event handlers
-
 window.addEventListener('DOMContentLoaded', init)
-
-
-
-// varaible = snake to move around the grid
-//object of the game is to move through each square on the grid without hitting the walls (end of grid)
-//player should be alerted if the snake hits the end of the grid with 'Game over' alert
-//event listener also needs to be added as and when the player hits any end of the grid 
-//snake will eat the food and increase in size which will increase its score
-//to increase difficulty i want to add an if/else statement that increases speed once a certain score is attained
-//will use the let variable (score) update the score of the game
-//will use the const dom varibale (score dis[play to to show the score)
-//framerate function can be used to move the snake around the screen at increasing speeds
-//will use the this.speed() function in order to increase and decrease the speed of the snake
-//A timer function will need to be added to the start of the game 
-//in order to give the player a countdown to when the game starts
-// may need to add a avriable/function/event listener that starts the countdown 
-//once the keycode (37,38,39 or 40) is pressed game 
-// function
-// case 39:
-//   if (snake % width < width - 1) {
-//     snake++
-//   }
-//   break
-// case 37:
-//   if (snake % width > 0) {
-//     snake--
-//   }
-//   break
-// case 40:
-//   if (playerIndex + width < width * width) {
-//     snake += width 
-//   }
-//   break
-// case 38:
-//   if (playerIndex - width >= 0) {
-//     snake -= width
-//   } 
-//   break
-// default:
-//if (snake >= squares.length) { 
-//snake = 0 
